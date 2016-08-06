f05ae232e29b4ccb4d491fe5aed4ac2f86aff3e9
EremeykinS <eremeykins@gmail.com>
2016-08-07 02:34:51 +0300

    questions button and commands

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ import telegram
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger('Maraphon_test_bot.' + __name__)
+logger = logging.getLogger('Marathon_test_bot.' + __name__)
 
-NAME, AGE, MAIN_MENU, MARATHON, QA, PAIN, ABOUT, ROUTE = range(8)
+NAME, AGE, MAIN_MENU, MARATHON, QA, PAIN, INFO, ROUTE, SELECT_CAT, HEALTH_Q, ANSWER = range(11)
 
 chat = dict()
 typing = telegram.ChatAction.TYPING
@@ -56,11 +56,13 @@ def main_menu(bot, update):
     elif chat[uid]['prev_state'] == MAIN_MENU:
         chat[uid]['prev_state'] = MAIN_MENU
         if ans not in flatten(main_kbd):
-            bot.sendMessage(uid, text="Ага %s, вопрос спрашиваешь!! постараюсь ответить, но в другой раз ахахах" % chat[uid]['name'], reply_markup=kbd(main_kbd))
+            bot.sendMessage(uid,
+                            text="Ага %s, вопрос спрашиваешь!! постараюсь ответить, но в другой раз ахахах" % chat[uid][
+                                'name'], reply_markup=kbd(main_kbd))
             return MAIN_MENU
 
 
-def about(bot, update):
+def info(bot, update):
     uid = update.message.from_user.id
     bot.sendChatAction(uid, action=typing)
     bot.sendMessage(uid, text=texts.info, reply_markup=kbd(main_kbd), parse_mode="HTML")
@@ -69,7 +71,9 @@ def about(bot, update):
 
 def qa(bot, update):
     uid = update.message.from_user.id
-    bot.sendMessage(uid, text="Тут будет множество кнопок со всякой фигней!", reply_markup=kbd(main_kbd))
+    bot.sendChatAction(uid, action=typing)
+    bot.sendMessage(uid, text=texts.select_cat, reply_markup=kbd(main_cat_kbd))
+    return SELECT_CAT
 
 
 def pain(bot, update):
@@ -77,7 +81,7 @@ def pain(bot, update):
     bot.sendMessage(uid, text="Тут будет все про БОЛЬ !", reply_markup=kbd(main_kbd))
 
 
-def marathon(bot, update):
+def about(bot, update):
     uid = update.message.from_user.id
     bot.sendChatAction(uid, action=typing)
     bot.sendMessage(uid, text=texts.about_marathon, reply_markup=kbd(marathon_kbd), parse_mode="HTML")
@@ -111,6 +115,85 @@ def distance_10(bot, update):
     bot.sendPhoto(uid, photo=img.route_10km, reply_markup=kbd(distance_kbd))
 
 
+def start_q(bot, update):
+    uid = update.message.from_user.id
+    bot.sendChatAction(uid, action=typing)
+    bot.sendMessage(uid, text=texts.select_q, reply_markup=kbd([[texts.first_time_q], [texts.back_to_all_cat_btn]]))
+    return ANSWER
+
+
+def health_cats(bot, update):
+    uid = update.message.from_user.id
+    bot.sendChatAction(uid, action=typing)
+    bot.sendMessage(uid, text=texts.select_sub_cat, reply_markup=kbd(health_cat_kbd), parse_mode="HTML")
+    return HEALTH_Q
+
+
+def health_q(bot, update):
+    uid = update.message.from_user.id
+    bot.sendChatAction(uid, action=typing)
+    ans = update.message.text
+    # text = texts.select_q
+    next_state = ANSWER
+    if ans == flatten(health_cat_kbd)[0]:
+        keyboard = health_c1_q_kbd
+    elif ans == flatten(health_cat_kbd)[1]:
+        keyboard = health_c2_q_kbd
+    elif ans == flatten(health_cat_kbd)[2]:
+        next_state = SELECT_CAT
+        # text = texts.select_cat
+        keyboard = main_cat_kbd
+    bot.sendMessage(uid, text=texts.select_q, reply_markup=kbd(keyboard))
+    return next_state
+
+
+def clothes_q(bot, update):
+    uid = update.message.from_user.id
+    bot.sendChatAction(uid, action=typing)
+    bot.sendMessage(uid, text=texts.select_q, reply_markup=kbd(clothes_q_kbd), parse_mode="HTML")
+    return ANSWER
+
+
+def regime_q(bot, update):
+    uid = update.message.from_user.id
+    bot.sendChatAction(uid, action=typing)
+    bot.sendMessage(uid, text=texts.select_q, reply_markup=kbd(regime_q_kbd), parse_mode="HTML")
+    return ANSWER
+
+
+def answer(bot, update):
+    uid = update.message.from_user.id
+    bot.sendChatAction(uid, action=typing)
+    ans = update.message.text
+    next_state = ANSWER
+    if ans == texts.first_time_q:
+        print('answering...')
+        text = texts.first_time_a
+        keyboard = [[texts.first_time_q], [texts.back_to_all_cat_btn]]
+    elif ans in texts.health_c1_q:
+        text = texts.health_c1_a[texts.health_c1_q.index(ans)]
+        keyboard = health_c1_q_kbd
+    elif ans in texts.health_c2_q:
+        text = texts.health_c2_a[texts.health_c2_q.index(ans)]
+        keyboard = health_c2_q_kbd
+    elif ans in texts.clothes_q:
+        text = texts.clothes_a[texts.clothes_q.index(ans)]
+        keyboard = clothes_q_kbd
+    elif ans in texts.regime_q:
+        text = texts.regime_a[texts.regime_q.index(ans)]
+        keyboard = regime_q_kbd
+    elif ans == texts.back_to_all_cat_btn:
+        next_state = SELECT_CAT
+        text = texts.select_cat
+        keyboard = main_cat_kbd
+    elif ans == flatten(health_c1_q_kbd)[-1]:
+        next_state = HEALTH_Q
+        text = texts.select_sub_cat
+        keyboard = health_cat_kbd
+    bot.sendMessage(uid, text=text, reply_markup=kbd(keyboard), parse_mode="HTML")
+    return next_state
+
+
 def helper(bot, update):
     bot.sendMessage(update.message.chat_id, text='Help!')
 
@@ -132,32 +215,52 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
+    command_handlers = [CommandHandler('start', start),
+                        CommandHandler('questions', qa),
+                        CommandHandler('about', about),
+                        CommandHandler('info', info)]
+
     # Add conversation handler with the states
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points=command_handlers,
 
         states={
             NAME: [MessageHandler([Filters.text], name)],
-            MAIN_MENU: [RegexHandler(main_kbd[0][0], marathon),
+            MAIN_MENU: [RegexHandler(main_kbd[0][0], about),
                         RegexHandler(main_kbd[0][1], qa),
                         RegexHandler(main_kbd[1][0], pain),
-                        RegexHandler(main_kbd[1][1], about),
-                        MessageHandler([Filters.text], main_menu)],
-            ABOUT: [MessageHandler([Filters.text], about)],
+                        RegexHandler(main_kbd[1][1], info),
+                        MessageHandler([Filters.text], main_menu)] + command_handlers,
+            INFO: [MessageHandler([Filters.text], info)] + command_handlers,
             MARATHON: [RegexHandler(flatten(marathon_kbd)[0], schedule),
-                        RegexHandler(flatten(marathon_kbd)[1], route),
-                        RegexHandler(flatten(marathon_kbd)[2], main_menu),
-                        MessageHandler([Filters.text], main_menu)],
+                       RegexHandler(flatten(marathon_kbd)[1], route),
+                       RegexHandler(flatten(marathon_kbd)[2], main_menu),
+                       MessageHandler([Filters.text], main_menu)] + command_handlers,
             ROUTE: [RegexHandler(distance_kbd[0][0], distance_42),
-                        RegexHandler(distance_kbd[0][1], distance_10),
-                        RegexHandler(distance_kbd[0][2], main_menu),
-                        MessageHandler([Filters.text], main_menu)]
+                    RegexHandler(distance_kbd[0][1], distance_10),
+                    RegexHandler(distance_kbd[0][2], main_menu),
+                    MessageHandler([Filters.text], main_menu)] + command_handlers,
+            SELECT_CAT: [RegexHandler(flatten(main_cat_kbd)[0], start_q),
+                         RegexHandler(flatten(main_cat_kbd)[1], health_cats),
+                         RegexHandler(flatten(main_cat_kbd)[2], clothes_q),
+                         RegexHandler(flatten(main_cat_kbd)[3], regime_q),
+                         RegexHandler(flatten(main_cat_kbd)[4], main_menu),
+                         MessageHandler([Filters.text], main_menu)] + command_handlers,
+            HEALTH_Q: [MessageHandler([Filters.text], health_q)] + command_handlers,
+            ANSWER: [MessageHandler([Filters.text], answer)] + command_handlers,
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
     )
 
+    # questions_handler = CommandHandler('questions', qa)
+    # about_handler = CommandHandler('about', about)
+    # info_handler = CommandHandler('info', info)
+
     dp.add_handler(conv_handler)
+    # dp.add_handler(questions_handler)
+    # dp.add_handler(about_handler)
+    # dp.add_handler(info_handler)
 
     # log all errors
     dp.add_error_handler(error)
--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,15 @@
+import texts
+from telegram import Emoji
+back_pic = Emoji.BACK_WITH_LEFTWARDS_ARROW_ABOVE
 main_menu_kbd = [["Главное меню"]]
-main_kbd = [["О марафоне", "Вопросы"], ["БОЛЬ", "О боте"]]
+main_kbd = [["О марафоне", "Частые вопросы"], ["Здоровье", "О Боте"]]
 yes_no_kbd = [['Да', 'Нет']]
 age_kbd = [["младше 18", "18-30"], ["30-45", "старше 45"]]
 marathon_kbd = [["Расписание"], ["Маршрут"], ["Главное меню"]]
 distance_kbd = [["42 км", "10 км", "Главное меню"]]
+main_cat_kbd = [["Начало тренировок", "Здоровье"], ["Одежда и обувь", "Режим тренировок"], ["Главное меню"]]
+health_cat_kbd = [["Заболевания"], ["Вес и мышцы"], [texts.back_to_all_cat_btn]]
+health_c1_q_kbd = [[q] for q in texts.health_c1_q] + [[texts.back_to_health_cat_btn]]
+health_c2_q_kbd = [[q] for q in texts.health_c2_q] + [[texts.back_to_health_cat_btn]]
+clothes_q_kbd = [[q] for q in texts.clothes_q] + [[texts.back_to_all_cat_btn]]
+regime_q_kbd = [[q] for q in texts.regime_q] + [[texts.back_to_all_cat_btn]]
